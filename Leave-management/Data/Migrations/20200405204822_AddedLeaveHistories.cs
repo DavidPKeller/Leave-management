222359a34a812c3f644185ad3aa0037a2870891e
Dave Keller <dave@servinet.co.za>
2020-04-13 22:11:40 +0200

    Added Leave TYpe Leave TYpe Repositories and Crud and DataTables and Font Awsome icons

---
+++ Leave-management/Data/Migrations/20200405204822_AddedLeaveHistories.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Leave_management.Data.Migrations
+{
+    public partial class AddedLeaveHistories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LeaveHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestingEmployeeID = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    LeaveTypeID = table.Column<int>(nullable: false),
+                    DateRequested = table.Column<DateTime>(nullable: false),
+                    DateActioned = table.Column<DateTime>(nullable: false),
+                    Approved = table.Column<bool>(nullable: true),
+                    ApprovedByID = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveHistories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveHistories_AspNetUsers_ApprovedByID",
+                        column: x => x.ApprovedByID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LeaveHistories_LeaveTypes_LeaveTypeID",
+                        column: x => x.LeaveTypeID,
+                        principalTable: "LeaveTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LeaveHistories_AspNetUsers_RequestingEmployeeID",
+                        column: x => x.RequestingEmployeeID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveHistories_ApprovedByID",
+                table: "LeaveHistories",
+                column: "ApprovedByID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveHistories_LeaveTypeID",
+                table: "LeaveHistories",
+                column: "LeaveTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveHistories_RequestingEmployeeID",
+                table: "LeaveHistories",
+                column: "RequestingEmployeeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeaveHistories");
+        }
+    }
+}
---
+++ Leave-management/Models/LeaveHistoryVM.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_management.Models
+{
+    public class LeaveHistoryVM
+    {
+
+        public int Id { get; set; }
+
+       public EmployeeVM RequestingEmployee { get; set; }
+        public string RequestingEmployeeID { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public DetailsLeaveTypeVM LeaveType { get; set; }
+        public int LeaveTypeID { get; set; }
+        
+        //D:  Bring in the list of Leave TYpes
+        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+        //D: ================================================================
+        public DateTime DateRequested { get; set; }
+        public DateTime DateActioned { get; set; }
+        public bool? Approved { get; set; }
+
+       
+        public EmployeeVM ApprovedBy { get; set; }
+        public string ApprovedByID { get; set; }
+    }
+}
